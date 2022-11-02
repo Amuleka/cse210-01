39dee2be287cf479a56a2b076e1570a766ee8b81
amuleka <amulek0297@outlook.com>
2022-11-02 13:54:03 -0600

    Testing new folder;

---
+++ madlibs.py
@@ -0,0 +1,23 @@
+print(" ")
+print("Please enter the following: ")
+print(" ")
+
+adjective = str(input("adjective: "))
+animal = str(input('animal: '))
+verb = str(input('verb:'))
+exclamation = str(input('exclamation: ')).capitalize()
+verb2 = str(input('verb2: '))
+verb3 = str(input('verb3: '))
+animal2 = str(input('animal2: '))
+adjective2 = str(input('adjective2: '))
+
+print(" ")
+print('Your story is:')
+print(" ")
+
+print(f"The other day, I was really in trouble. It all started when I saw a very \
+{adjective} {animal} {verb} down the hallway. {exclamation}! I yelled. But all \
+I could think to do was to {verb2} over and over. Miraculously, \
+that caused it to stop, but not before it tried to {verb3}.On the next day, the {animal2} was {adjective2} because of what happened yesterday")
+
+print(" ")
