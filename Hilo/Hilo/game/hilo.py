389a8c92de0a96cfc84a946de05758930b05d313
amuleka <amulek0297@outlook.com>
2022-11-10 14:35:07 -0600

    new changes

--- Hilo/Hilo/game/hilo.py
+++ Hilo/Hilo/game/hilo.py
@@ -14,25 +14,3 @@ class Hilo:
 # 2) Create the class constructor. Use the following method comment.
 
     def __init__(self):
-        """Constructs a new instance of Die with a value and points attribute.
-        Args:
-            self (Die): An instance of Die.
-        """
-        self.value = 0
-        self.points = 0
-
-# 3) Create the roll(self) method. Use the following method comment.
-    def roll(self):
-        """Generates a new random value and calculates the points.
-        
-        Args:
-            self (Die): An instance of Die.
-        """
-        self.value = random.randint(1,6)
-        if self.value == 1:
-            self.points = 100
-        elif self.value == 5:
-                self.points = 50
-        else:
-            self.points = 0
-        
